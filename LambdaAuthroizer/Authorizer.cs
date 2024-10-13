f2d88d56155161a0aeecbc1d3256a48cd453f7d9
tech4m <amitleo81@gmail.com>
2024-10-13 19:44:45 +0530

    Updated code for AWS Lambda Authorizer

---
+++ LambdaAuthroizer/Authorizer.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace LambdaAuthroizer;
+
+public class Authorizer
+{
+    public async Task<APIGatewayCustomAuthorizerResponse> Auth(APIGatewayCustomAuthorizerRequest request)
+    {
+        var response = new APIGatewayCustomAuthorizerResponse();
+        var idToken = request.AuthorizationToken;
+        var idTokenDetails = new JwtSecurityToken(jwtEncodedString: idToken);
+
+        var kid = idTokenDetails.Header["kid"].ToString();
+        var issuer = idTokenDetails.Claims.First(x => x.Type == "iss").Value;
+        var audience = idTokenDetails.Claims.First(x => x.Type == "aud").Value;
+
+
+        return response;
+    }
+}
