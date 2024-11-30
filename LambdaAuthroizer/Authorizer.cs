10137101ecd48dafb3d804edc811e2785748616a
tech4m <amitleo81@gmail.com>
2024-11-30 19:58:05 +0530

    updated code

--- LambdaAuthroizer/Authorizer.cs
+++ LambdaAuthroizer/Authorizer.cs
@@ -1,5 +1,9 @@
 ﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.SecretsManager;
 using System.IdentityModel.Tokens.Jwt;
+using Amazon.SecretsManager.Model;
+using System.Text.Json;
+using Microsoft.IdentityModel.Tokens;
 
 namespace LambdaAuthroizer;
 
@@ -7,14 +11,81 @@ public class Authorizer
 {
     public async Task<APIGatewayCustomAuthorizerResponse> Auth(APIGatewayCustomAuthorizerRequest request)
     {
-        var response = new APIGatewayCustomAuthorizerResponse();
-        var idToken = request.AuthorizationToken;
-        var idTokenDetails = new JwtSecurityToken(jwtEncodedString: idToken);
+
+
+        var idToken = request.QueryStringParameters["token"];
+        Console.WriteLine($"Token is {idToken}");
+        var idTokenDetails = new JwtSecurityToken(idToken);
 
         var kid = idTokenDetails.Header["kid"].ToString();
         var issuer = idTokenDetails.Claims.First(x => x.Type == "iss").Value;
         var audience = idTokenDetails.Claims.First(x => x.Type == "aud").Value;
+        var userId = idTokenDetails.Claims.First(x => x.Type == "sub").Value;
 
+        var response = new APIGatewayCustomAuthorizerResponse()
+        {
+            PrincipalID = userId,
+            PolicyDocument = new APIGatewayCustomAuthorizerPolicy()
+            {
+                Version = "2012-10-17",
+                Statement = new List<APIGatewayCustomAuthorizerPolicy.IAMPolicyStatement>()
+                {
+                    new APIGatewayCustomAuthorizerPolicy.IAMPolicyStatement()
+                    {
+                        Action = new HashSet<string>(){"execute-api:Invoke"},
+                        Effect = "Allow",
+                        Resource = new HashSet<string>(){request.MethodArn}
+                    }
+                }
+            }
+        };
+
+
+        var secretsClient = new AmazonSecretsManagerClient();
+        var secret = await secretsClient.GetSecretValueAsync(new GetSecretValueRequest
+        {
+            SecretId = "hotelCognitoKey"
+        });
+
+        var privateKeys = secret.SecretString;
+
+        Console.WriteLine($"JWKS set: {privateKeys}");
+        var jwks = new JsonWebKeySet(privateKeys);
+
+        foreach (var key in jwks.Keys)
+        {
+            Console.WriteLine(key.Kid);
+        }
+        var privateKey = jwks.Keys.First(x => x.Kid == kid);
+
+        var handler = new JwtSecurityTokenHandler();
+        var result = await handler.ValidateTokenAsync(idToken, new TokenValidationParameters
+        {
+            ValidIssuer = issuer,
+            ValidAudience = audience,
+            IssuerSigningKey = privateKey
+        });
+
+        if (!result.IsValid) throw new UnauthorizedAccessException("Token not valid");
+
+        var apiGroupMapping = new Dictionary<string, string>()
+        {
+            {"listadminshotel+", "Admin"},
+            {"admin+", "Admin"}
+        };
+
+        var expectdGroup = apiGroupMapping.FirstOrDefault(x =>
+            request.Path.Contains(x.Key, StringComparison.InvariantCultureIgnoreCase));
+
+        if (!expectdGroup.Equals(default(KeyValuePair<string, string>)))
+        {
+            var userGroup = idTokenDetails.Claims.First(x => x.Type == "cognito:groups").Value;
+            if (string.Compare(userGroup, expectdGroup.Value,
+                    StringComparison.InvariantCultureIgnoreCase) != 0)
+            {
+                response.PolicyDocument.Statement[0].Effect = "Deny";
+            }
+        }
 
         return response;
     }
